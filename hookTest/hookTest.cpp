27824349cf892150538a5ed91ea1d528d481d053
longlongwaytogo <longlongwaytogo@users.noreply.github.com>
2023-05-08 10:29:25 +0800

    1 支持vs2019编译 2 添加minhook测试程序

---
+++ hookTest/hookTest.cpp
@@ -0,0 +1,91 @@
+﻿#include <Windows.h>
+#include "MinHook.h"
+
+#if defined _M_X64
+#pragma comment(lib, "libMinHook.x64.lib")
+#elif defined _M_IX86
+#pragma comment(lib, "libMinHook.x86.lib")
+#endif
+
+typedef int (WINAPI* MESSAGEBOXW)(HWND, LPCWSTR, LPCWSTR, UINT);
+
+// Pointer for calling original MessageBoxW.
+MESSAGEBOXW fpMessageBoxW = NULL;
+
+// Detour function which overrides MessageBoxW.
+int WINAPI DetourMessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType)
+{
+    return fpMessageBoxW(hWnd, L"Hooked!", lpCaption, uType);
+}
+
+template <typename T>
+inline MH_STATUS MH_CreateHookEx(LPVOID pTarget, LPVOID pDetour, T** ppOriginal)
+{
+    return MH_CreateHook(pTarget, pDetour, reinterpret_cast<LPVOID*>(ppOriginal));
+}
+
+template <typename T>
+inline MH_STATUS MH_CreateHookApiEx(
+    LPCWSTR pszModule, LPCSTR pszProcName, LPVOID pDetour, T** ppOriginal)
+{
+    return MH_CreateHookApi(
+        pszModule, pszProcName, pDetour, reinterpret_cast<LPVOID*>(ppOriginal));
+}
+
+
+int main()
+{
+    // Initialize MinHook.
+    if (MH_Initialize() != MH_OK)
+    {
+        return 1;
+    }
+#if 0
+    // Create a hook for MessageBoxW, in disabled state.
+    if (MH_CreateHook(&MessageBoxW, &DetourMessageBoxW,
+        reinterpret_cast<LPVOID*>(&fpMessageBoxW)) != MH_OK)
+    {
+        return 1;
+    }
+
+#else
+
+    // Create a hook for MessageBoxW, in disabled state.
+    if (MH_CreateHookApiEx(L"user32", "MessageBoxW", &DetourMessageBoxW, &fpMessageBoxW) != MH_OK)
+    {
+        return 1;
+    }
+#endif 
+    // or you can use the new helper function like this.
+    //if (MH_CreateHookApiEx(
+    //    L"user32", "MessageBoxW", &DetourMessageBoxW, &fpMessageBoxW) != MH_OK)
+    //{
+    //    return 1;
+    //}
+
+    // Enable the hook for MessageBoxW.
+    if (MH_EnableHook(&MessageBoxW) != MH_OK)
+    {
+        return 1;
+    }
+
+    // Expected to tell "Hooked!".
+    MessageBoxW(NULL, L"Not hooked...", L"MinHook Sample", MB_OK);
+
+    // Disable the hook for MessageBoxW.
+    if (MH_DisableHook(&MessageBoxW) != MH_OK)
+    {
+        return 1;
+    }
+
+    // Expected to tell "Not hooked...".
+    MessageBoxW(NULL, L"Not hooked...", L"MinHook Sample", MB_OK);
+
+    // Uninitialize MinHook.
+    if (MH_Uninitialize() != MH_OK)
+    {
+        return 1;
+    }
+
+    return 0;
+}
